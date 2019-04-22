dedc6ac96be3619ce3706b1179f598687d39e9eb
dalandev <dalanniha@gmail.com>
2019-04-22 23:21:12 +0430

    initial import

--- app/src/main/java/ir/mrdota/MainActivity.java
+++ app/src/main/java/ir/mrdota/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+///rrrrr
         setContentView(R.layout.login);
     }
 }
